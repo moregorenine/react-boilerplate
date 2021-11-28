8ecede48facb38178305e903f5d1bc42479b9aae
moregorenine <moregorenine@gmail.com>
2021-11-28 18:36:28 +0900

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './common/bootstrap'
 
-import HelloWorld from './reactjs.org/1.hello-world/HelloWorld'
-import Jsx from './reactjs.org/2.jsx/Jsx'
-import RenderingElements from './reactjs.org/3.rendering-elements/RenderingElements'
-import Welcome from './reactjs.org/4.components-props/Welcome'
-import Clock from './reactjs.org/5.state-lifecycle/Clock'
-import Form from './reactjs.org/6.handling-events/Form'
-import Toggle from './reactjs.org/6.handling-events/Toggle'
-import LoginControl from './reactjs.org/7.conditional-rendering/LoginControl'
-import Mailbox from './reactjs.org/7.conditional-rendering/Mailbox'
+import HelloWorld from './reactjs.org/main-concepts/1.hello-world/HelloWorld'
+import Jsx from './reactjs.org/main-concepts/2.jsx/Jsx'
+import RenderingElements from './reactjs.org/main-concepts/3.rendering-elements/RenderingElements'
+import Welcome from './reactjs.org/main-concepts/4.components-props/Welcome'
+import Clock from './reactjs.org/main-concepts/5.state-lifecycle/Clock'
+import Form from './reactjs.org/main-concepts/6.handling-events/Form'
+import Toggle from './reactjs.org/main-concepts/6.handling-events/Toggle'
+import LoginControl from './reactjs.org/main-concepts/7.conditional-rendering/LoginControl'
+import Mailbox from './reactjs.org/main-concepts/7.conditional-rendering/Mailbox'
 // import Counter from './state/Counter'
 
 const App = () => {
